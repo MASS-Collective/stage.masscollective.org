fe768541c84b71d07094feb1601b51e9a44126ca
Jeremy Sewell <jeremys@jeremys-imac.local>
2014-05-21 11:24:23 -0400

    convert mass site to static html

---
+++ lib/process-email.php
@@ -0,0 +1,48 @@
+<?php 
+$errors         = array();    // array to hold validation errors
+$data       = array();    // array to pass back data
+
+// validate the variables ======================================================
+  // if any of these variables don't exist, add an error to our $errors array
+
+  if (empty($_POST['name']))
+    $errors['name'] = 'Name is required.';
+
+  if (empty($_POST['email']))
+    $errors['email'] = 'Email is required.';
+
+  if (empty($_POST['contactmessage']))
+    $errors['contactmessage'] = 'Superhero alias is required.';
+
+// return a response ===========================================================
+
+  // if there are any errors in our errors array, return a success boolean of false
+  if ( ! empty($errors)) {
+
+    // if there are items in our errors array, return those errors
+    $data['success'] = false;
+    $data['errors']  = $errors;
+  } else {
+
+    // if there are no errors process our form, then return a message
+
+    // DO ALL YOUR FORM PROCESSING HERE
+    require_once('send.php');
+    Send::mail()->
+    to('j3therevenge@gmail.com')->
+    from($_POST['email'])->
+    fromName($_POST['name'])->
+    subject('MASS Collective contact form')->
+    message($_POST['contactmessage'])->send();
+
+    // THIS CAN BE WHATEVER YOU WANT TO DO (LOGIN, SAVE, UPDATE, WHATEVER)
+
+    // show a message of success and provide a true success variable
+    $data['success'] = true;
+    $data['message'] = 'Success!';
+  }
+
+  // return all our data to an AJAX call
+  echo json_encode($data);
+
+?>
